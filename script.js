193960f7bb320c17a309e3eb750a04b16747f4e2
unknown <you@example.com>
2022-08-08 17:57:10 +0600

    store data successfully

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let form = document.getElementById("form")
 let textInput = document.getElementById("textInput")
+let msg = document.getElementById("msg")
+let dateInput = document.getElementById("dateInput")
+let textarea = document.getElementById("textarea")
 
 form.addEventListener("click",(e)=>{
     e.preventDefault()
@@ -9,11 +12,24 @@ form.addEventListener("click",(e)=>{
 
 let formValidation = () =>{
     if(textInput.value===""){
-        console.log("please enter your text");
+        msg.innerHTML = "input can not be blank"
     }
     else{
         console.log("success");
+        msg.innerHTML = ""
+        acceptData()
     }
 }
 
-//1.28
+let data = {}
+
+let acceptData = () =>{
+    data["text"] = textInput.value
+    dateInput["date"] = dateInput.value
+    textarea["description"] = textarea.value
+
+    console.log(data);
+
+}
+
+//1.36
