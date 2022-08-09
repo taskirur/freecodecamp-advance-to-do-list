2c591787a7c77c0364a12adc7f697fe63b2c4759
unknown <you@example.com>
2022-08-09 12:19:21 +0600

    delete part hast been completed also added the ; in javascript part

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let form = document.getElementById("form")
-let textInput = document.getElementById("textInput")
-let msg = document.getElementById("msg")
-let dateInput = document.getElementById("dateInput")
-let textarea = document.getElementById("textarea")
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let msg = document.getElementById("msg");
+let textarea = document.getElementById("textarea");
+let tasks = document.getElementById("tasks");
+let addBtn = document.getElementById("addBtn");
 
-form.addEventListener("click",(e)=>{
+
+form.addEventListener("submit",(e)=>{
     e.preventDefault()
     formValidation()
 
-})
+});
 
 let formValidation = () =>{
     if(textInput.value===""){
@@ -16,20 +19,51 @@ let formValidation = () =>{
     }
     else{
         console.log("success");
-        msg.innerHTML = ""
-        acceptData()
-    }
-}
+        msg.innerHTML = "";
+        acceptData();
 
-let data = {}
+        addBtn.setAttribute("data-bs-dismiss", "modal")
+        addBtn.click();
+
+        (()=>{
+            addBtn.setAttribute("data-bs-dismiss","")
+
+        })()
+    }
+};
+
+let data = {};
 
 let acceptData = () =>{
-    data["text"] = textInput.value
-    dateInput["date"] = dateInput.value
-    textarea["description"] = textarea.value
-
+    data["text"] = textInput.value;
+    data["date"] = dateInput.value;
+    data["description"] = textarea.value;
     console.log(data);
+    createTask();
 
 }
 
-//1.36
+let createTask = () =>{
+    tasks.innerHTML += `<div>
+    <span>${data.text}</span>
+    <span>${data.date}</span>
+    <p>${data.description}</p>
+
+    <span class="options">
+        <i class="fa-solid fa-pen-to-square"></i>
+        <i onClick="deleteTask(this)" class="fa-solid fa-delete-left"></i>
+    </span>
+    </div>`
+    restForm()
+}
+
+let restForm = () =>{
+     textInput.value = "";
+     dateInput.value = "";
+     textarea.value = "";
+}
+
+let deleteTask = (e) =>{
+    e.parentElement.parentElement.remove()
+    console.log("task delete");
+}
